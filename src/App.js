bc2afe0d5003d13163849c683495966d838f8ca7
Griffin Muteti <griffinmuteti31@gmail.com>
2023-01-18 18:02:22 +0300

    route component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import ComponentView from './components/ComponentView';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ComponentView from "./components/ComponentView";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('login');
-  //how to create react app.js with navigation ?
-  
+  const [currentPage, setCurrentPage] = useState("login");
+
   return (
-    <div className="bg-gray-200 min-h-screen flex">
-      <Header/>
-      <Router>
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <div className="w-full flex-1 p-4">
-        <ComponentView currentPage={currentPage} />
+    <Router>
+      <div className="bg-gray-200 min-h-screen flex">
+        <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        <Routes>
+          <Route
+            path="/"
+            element={<ComponentView currentPage={currentPage} />}
+          />
+        </Routes>
       </div>
-      </Router>
-      <Footer/>
-    </div>
+    </Router>
   );
 }
 
